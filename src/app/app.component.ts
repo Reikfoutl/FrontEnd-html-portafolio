b37c272d466dd29463d79a4861df82335d512fef
Reikfoutl <ri0irispurpura@gmail.com>
2023-04-16 10:11:51 -0300

    portafolioNuevohtml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,87 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portafolio';
+  forma!:FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.crearFormulario();
+  }
+
+get nombreNoValido(){
+  return this.forma.get('nombre')?.invalid && this.forma.get('nombre')?.touched;
+}  
+
+get apellidoNoValido(){
+  return this.forma.get('apellido')?.invalid && this.forma.get('apellido')?.touched;
+}
+
+get correoNoValido(){
+  return this.forma.get('correo')?.invalid && this.forma.get('correo')?.touched;
+}
+
+get passwordNoValido(){
+  return this.forma.get('password')?.invalid && this.forma.get('password')?.touched;
+}
+
+get passwordNoValido2(){
+  return this.forma.get('password2')?.invalid && this.forma.get('password2')?.touched;
+}
+  crearFormulario(){
+    this.forma = this.fb.group({
+      nombre:['', Validators.required],
+      apellido:['', Validators.required],
+      correo:['', Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')],
+      contraseña:['', Validators.required, Validators.minLength(5), Validators.maxLength( 12)]
+    },{
+      Validators:this.passwordIguales('password', 'password2')
+    })
+  }
+
+  
+
+  passNovalido(passName:string, passName2:string){
+    const pass1 = this.forma.get('password')?.value;
+    const pass2= this.forma.get('password2')?.value;
+  if (pass1 !== pass2 ) {
+    return true;
+  }else{
+    return false;
+  }  
+  }
+
+  guardar(){
+    console.log(this.forma);
+
+    this.passNovalido;
+
+     if (this.forma.invalid){
+       
+      return Object.values(this.forma.controls).forEach(control =>{
+
+        control.markAllAsTouched();
+
+      })
+     }
+  }
+
+  limpiar(){
+    this.forma.reset();
+    
+    
+  }
+  
+
+  passwordIguales(passName:string, passName2:string){
+    return (formGroup:FormGroup) => {
+      const passControl = formGroup.get(passName);
+      const pass2Control = formGroup.get(passName2);
+
+    if (passControl?.value === pass2Control?.value){
+      pass2Control?.setErrors(null);
+    }else{
+      pass2Control?.setErrors({noEsIgual:true})
+    }  
+    }
+  }
+
 }
